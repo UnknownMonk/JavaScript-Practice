85d23922808189e03bfcd299cbfe32258e45edd4
UnknownMonk <smodcartoon@gmail.com>
2018-06-03 16:37:31 -0400

    How to use a iifes

---
+++ IIFEs.js
@@ -0,0 +1,10 @@
+/*
+
+Q:What is a IIFEs and how is it used 
+A: A IIFEs is a imeadeatly invoked function expression aka a function  the is called and soon as it is ran. 
+*/
+
+
+(function() {
+  alert("Woohoo!");
+})();
