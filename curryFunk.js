b2acbd35fbf0a781c0d5942a436594894cb56f04
UnknownMonk <smodcartoon@gmail.com>
2018-06-05 09:13:24 -0400

    correction

--- curryFunk.js
+++ curryFunk.js
@@ -1,12 +1,36 @@
 //Curry this function.
 
-function getProduct(num1){
-  return function(num2){
-    return num1 + num2;
-  }
-}
-
-console.log(getProduct(10)(20));
+// tab 1
  
+// function getProduct(num1, num2) {
+//   return num1 * num2;
+// }
+ 
+function getProduct(num1) {
+  return function(num2) {
+    return num1 * num2;
+  };
+}
+ 
+getProduct(10)(20);
+ 
+ 
+ 
+ 
+// tab 2
+ 
+// function getTravelTime(distance, speed) {
+//   return distance / speed;
+// }
+ 
+function getTravelTime(distance) {
+  return function(speed) {
+    return distance / speed;
+  };
+}
+ 
+const travelTimeBosNyc = getTravelTime(400);
+const travelTimeMiamiAtlanta = getTravelTime(600);
+console.log(travelTimeBosNyc(100));
 
 
