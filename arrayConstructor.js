6c8aa8201228b1549f0d70934fc7e11660dd7c04
UnknownMonk <smodcartoon@gmail.com>
2018-06-09 10:06:39 -0400

    array constructor function and how it works

---
+++ arrayConstructor.js
@@ -0,0 +1,21 @@
+/** 
+ * What is loged out for each array..
+ * 
+ * 
+*/
+
+
+
+
+
+
+
+var arr1 = [];
+var arr2 = new Array(50);
+var arr3 = new Array(1, 2, "three", 4, "five");
+var arr4 = new Array([1, 2, 3, 4, 5]);
+
+console.log("arr1: ", arr1); // []
+console.log("arr2: ", arr2);// [<50 empty items>]
+console.log("arr3: ", arr3);// [1,2,"three", 4, "five"]
+console.log("arr4: ", arr4);// [[1,2,3,4,5]]
