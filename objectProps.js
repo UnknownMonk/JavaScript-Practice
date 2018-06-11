4deaa6b00b3bcf827eb4f097f1612538538ba71f
UnknownMonk <smodcartoon@gmail.com>
2018-06-11 08:48:23 -0400

    na

--- objectProps.js
+++ objectProps.js
@@ -1,8 +1,15 @@
+
+
+
 const a = {};
-const b = { name: "b" };
-const c = { name: "c" };
-
+const b = { name: 'b' };
+const c = { name: 'c' };
+ 
 a[b] = 200;
+// a['[object Object]'] = 200
+ 
 a[c] = 400;
-
+// a['[object Object]'] = 400
+ 
 console.log(a[b]);
+// console.log(a['[object Object]']);
